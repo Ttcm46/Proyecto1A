77348fbcd4329689451a55f4f7793e7ce1c7c868
Pablo RZ <paporuiz08@gmail.com>
2023-05-23 23:52:40 -0600

    Stats & death imgs

--- Proyecto1A/Proyecto1A.cpp
+++ Proyecto1A/Proyecto1A.cpp
@@ -53,8 +53,8 @@ typedef struct Enemigo {
 	int hitboxX = 50;
 	int hitboxY = 50;
 	Enemigo* siguiente = NULL;
-	unsigned int lifeTime = 9500000;
-	unsigned int deathDraw = 5000;
+	unsigned int lifeTime = 500;
+	unsigned int deathDraw = 100;
 }*PtrEnemigo;
 
 const float FPS = 20.0;	//fps 
@@ -89,8 +89,16 @@ void draw(ALLEGRO_BITMAP* bullet) {
 	}
 	PtrEnemigo tmp = enemigos;
 	while (tmp) {
-		if(tmp->estado)
+		if(tmp->estado||tmp->death)
 		al_draw_bitmap(imgEnems[tmp->codigoimagen][tmp->death], (tmp->x-(offsetx-600)) - tmp->hitboxX, tmp->y - tmp->hitboxY, 0);
+		if (tmp->death)
+		{
+			tmp->deathDraw -= 1;
+			if (tmp->deathDraw == 0)
+			{
+				tmp->death = false;
+			}
+		}
 		tmp = tmp->siguiente;
 	}
 }
@@ -147,24 +155,35 @@ PtrEnemigo accessEnem(int n) {
 
 void spawnEnems() {
 	PtrEnemigo a = (accessEnem(rand() % 5));
-	if ((a)&&(!a->estado) && (rand() % 2)) {
+	if ((a)&&(!a->estado)&&(!a->death) && (rand() % 2)) {
 		a->x = (rand() % ((2000 - a->hitboxX) - (a->hitboxX) + 1)) + a->hitboxX;
 		a->y = (rand() % ((800 - a->hitboxY) - a->hitboxY + 1)) + a->hitboxY;
 		a->estado = true;
 		a->codigoimagen = rand() % 2;
+		a->lifeTime = 500;
+		a->death = false;
+		a->deathDraw = 100;
 	}
 }
-void bulletHits(ALLEGRO_DISPLAY* display) {		//TODO: finish
+bool bulletHits(ALLEGRO_DISPLAY* display) {		//TODO: finish
 	PtrBala tmp = balas;
 	PtrEnemigo tmpEnem= enemigos;
 	while (tmp){
 		//espresion para comprobar impactos;
 		//std::cout << "condX: " << (actual->x + actual->hitBoxX > tmp->x - tmp->hurtboxX && actual->x - actual->hitBoxX < tmp->x + tmp->hurtboxX) << " condY: " << (actual->y + actual->hitBoxY > tmp->y - tmp->hurtboxY && actual->y - actual->hitBoxY < tmp->y + tmp->hurtboxY) << std::endl;
 		while (tmpEnem) {
+			if (tmpEnem->estado) tmpEnem->lifeTime -= 1;;
+			if (tmpEnem->lifeTime == 0) {
+				tmpEnem->estado = false;
+				tmpEnem->death = false;
+				actual->missed += 1;
+			}
 			if (tmp->estado&&tmp->playerShot && tmp->estado && (tmpEnem->x + tmpEnem->hitboxX > tmp->x - tmp->hurtboxX && tmpEnem->x - tmpEnem->hitboxX < tmp->x + tmp->hurtboxX)&& (tmpEnem->y + tmpEnem->hitboxY > tmp->y - tmp->hurtboxY && tmpEnem->y - tmpEnem->hitboxY < tmp->y + tmp->hurtboxY)) {
 				actual->enemigosDestruidos += 1;
 				tmpEnem->estado = false;
+				tmpEnem->death = true;
 				tmp->estado = false;
+
 
 				//TODO: comprobar funcionamiento
 			}
@@ -178,32 +197,40 @@ void bulletHits(ALLEGRO_DISPLAY* display) {		//TODO: finish
 				}
 				al_show_native_message_box(display, ":(", " ", "Ha perdido una vida", "Acepto", NULL);
 				offsetx = 1000;
-				return;
+				return true;
 			}
 			tmpEnem = tmpEnem->siguiente;
 		}
 		tmp = tmp->suiguiente;
 	}
+	return false;
 }
 void enemyShoots() {
 	PtrEnemigo a = (accessEnem(rand() % 5));
-	if (rand() % 21 == 0)				//la cigarras deben saber lo quee hacen si cada 21 anhos salen y casi no se encuentran a otras especies de cigarras
+	if (rand() % 21 == 0&&a->estado&&!a->death)				//la cigarras deben saber lo quee hacen si cada 21 anhos salen y casi no se encuentran a otras especies de cigarras
 		disparar(a->x, a->y, ((a->x-actual->x)<0) ? (false) : (true), false);
 }
-void gameLogic(ALLEGRO_BITMAP* bullet, ALLEGRO_BITMAP* ship,PtrNave actual, ALLEGRO_DISPLAY* display) {
-	bulletHits(display);
+bool gameLogic(ALLEGRO_BITMAP* bullet, ALLEGRO_BITMAP* ship,PtrNave actual, ALLEGRO_DISPLAY* display) {
+	if (bulletHits(display)) {
+		return true;
+	}
 	eliminarBalas();
 	draw(bullet);
 	drawLifes(ship);
 	spawnEnems();
 	enemyShoots();
-
+	return false;
 }
 
 void mainGame(ALLEGRO_DISPLAY* display,ALLEGRO_BITMAP* background,ALLEGRO_EVENT_QUEUE* cola_eventos) {
 	actual = (vidas->estado) ? (vidas) : ((vidas->siguiente->estado) ? (vidas->siguiente) : ((vidas->siguiente->siguiente->estado) ? (vidas->siguiente->siguiente) : (NULL)));
 	if (!actual) {
 		al_show_native_message_box(display, "Error", "NO tiene mas vidas", "No tiene mas vidas", "Acepto", NULL);
+		return;
+	}
+	ALLEGRO_FONT* fontB = al_load_font("SuperMario256.ttf", 20, NULL);
+	if(!fontB) {
+		al_show_native_message_box(display, "Error", "Su maquina no tiene una de las fuentes necesarias para cargar", "Una fuente no se pudo cargar", "Acepto", NULL);
 		return;
 	}
 	background = al_load_bitmap("DefenderMap.png");
@@ -298,13 +325,16 @@ void mainGame(ALLEGRO_DISPLAY* display,ALLEGRO_BITMAP* background,ALLEGRO_EVENT_
 		if (right) { actual->x += actual->velocidad; offsetx += actual->velocidad; ship = ship_R; }
 		al_draw_bitmap(background, -1*(offsetx-600), 0, 0);
 		al_draw_bitmap(ship, 550, actual->y-50, 0);
+		al_draw_textf(fontB, al_map_rgb(255, 0, 255), 1025, 0, ALLEGRO_ALIGN_CENTRE, "Disparos: %d Destruidos: %d", actual->disparos, actual->enemigosDestruidos);
 		if (events.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN||(events.type== ALLEGRO_EVENT_KEY_DOWN&& events.keyboard.keycode==75)) {
 			disparar((actual->x+((ship==ship_L)?(-1*actual->hitBoxX):(actual->hitBoxX))), actual->y, (ship == ship_L) ? (true) : (false), true);
 			actual->disparos += 1;
 			std::cout << "Nave x: " << actual->x << " y: " << actual->y << " Disparos: " << actual->disparos << " hits: " << actual->enemigosDestruidos << std::endl;
 			//std::cout << "Nave x: " << actual->x << " y: " << actual->y << " Disparos: " << actual->disparos << std::endl;
 		}
-		gameLogic(bullet, ship_L,actual,display);
+		if (gameLogic(bullet, ship_L, actual, display)) {
+			return;
+		}
 		al_flip_display();
 
 		//TODO: cerrar todas la imagenes para no tener tanta basura por ahi
@@ -316,14 +346,42 @@ void mainGame(ALLEGRO_DISPLAY* display,ALLEGRO_BITMAP* background,ALLEGRO_EVENT_
 	al_destroy_bitmap(ship_R);
 
 }
+void stats(ALLEGRO_DISPLAY* display, ALLEGRO_EVENT_QUEUE* cola_eventos) {
+
+	ALLEGRO_FONT* fontB = al_load_font("SuperMario256.ttf", 24, NULL);
+
+	al_clear_to_color(al_map_rgb(0, 0, 0));
+
+	al_draw_textf(fontB, al_map_rgb(255, 0, 255), 200, 10, ALLEGRO_ALIGN_CENTRE, "Vida 1:");
+	al_draw_textf(fontB, al_map_rgb(255, 0, 255), 600, 110, ALLEGRO_ALIGN_CENTRE, "Disparos: %d       Destruidos: %d       Fallos: %d",vidas->disparos,vidas->enemigosDestruidos,vidas->missed);
+
+	al_draw_textf(fontB, al_map_rgb(255, 0, 255), 200, 273, ALLEGRO_ALIGN_CENTRE, "Vida 2:");
+	al_draw_textf(fontB, al_map_rgb(255, 0, 255), 600, 373, ALLEGRO_ALIGN_CENTRE, "Disparos: %d       Destruidos: %d       Fallos: %d", vidas->siguiente->disparos, vidas->siguiente->enemigosDestruidos, vidas->siguiente->missed);
+
+	al_draw_textf(fontB, al_map_rgb(255, 0, 255), 200,536, ALLEGRO_ALIGN_CENTRE, "Vida 3:");
+	al_draw_textf(fontB, al_map_rgb(255, 0, 255), 600, 636, ALLEGRO_ALIGN_CENTRE, "Disparos: %d       Destruidos: %d       Fallos: %d", vidas->siguiente->siguiente->disparos, vidas->siguiente->siguiente->enemigosDestruidos, vidas->siguiente->siguiente->missed);
+
+	al_flip_display();
+
+	while (true) {
+		ALLEGRO_EVENT events;
+		al_wait_for_event(cola_eventos, &events);
+		if (events.type == ALLEGRO_EVENT_KEY_DOWN) {
+			if (events.keyboard.keycode == 59)
+				return;
+		}
+
+	}
+}
 
 void menu(ALLEGRO_DISPLAY* display, ALLEGRO_BITMAP* background, ALLEGRO_EVENT_QUEUE* cola_eventos, ALLEGRO_TIMER* timer) {
 	background = al_load_bitmap("Fondo.png");
-	ALLEGRO_BITMAP* back_A, *back_B, *back_C,*back_D;
+	ALLEGRO_BITMAP* back_A, *back_B, *back_C,*back_D,*back_E;
 	back_A = background;
 	back_B = al_load_bitmap("Fondo_Opc_1.png");
 	back_C = al_load_bitmap("Fondo_Opc_2.png");
 	back_D = al_load_bitmap("Fondo_Opc_3.png");
+	back_E = al_load_bitmap("Fondo_Opc_4.png");
 	int mousex = 0;
 	int mousey = 0;
 	if (!background&&!back_A&&!back_B&&!back_C&&!back_D)
@@ -342,14 +400,17 @@ void menu(ALLEGRO_DISPLAY* display, ALLEGRO_BITMAP* background, ALLEGRO_EVENT_QU
 			mousey = eventos.mouse.y;
 		}
 		if (eventos.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP) {
-			if (mousex > 500 && mousey > 386 && mousex < 718 && mousey < 434) {
+			if (mousex > 497 && mousey > 337 && mousex < 719 && mousey < 384) {
 				mainGame(display, background, cola_eventos);
 			}
-			else if (mousex > 418 && mousey > 499 && mousex < 741 && mousey < 547) {
+			else if (mousex > 481 && mousey > 440 && mousex < 737 && mousey < 487) {
 				//TODO: opcion de cargar partida aqui;
 				al_show_native_message_box(display, "Error", "Opcion aun no implementada", "Esta opcion aun no se implementa", "Acepto", NULL);
 			}
-			else if (mousex > 516 && mousey > 607 && mousex < 713 && mousey < 655) {
+			else if (mousex > 384 && mousey > 541 && mousex < 860 && mousey < 589) {
+				stats(display,cola_eventos);
+			}
+			else if (mousex > 521 && mousey > 648 && mousex < 719 && mousey < 696) {
 				al_destroy_bitmap(back_A);
 				al_destroy_bitmap(back_B);
 				al_destroy_bitmap(back_C);
@@ -358,7 +419,7 @@ void menu(ALLEGRO_DISPLAY* display, ALLEGRO_BITMAP* background, ALLEGRO_EVENT_QU
 			}
 			
 		}
-		background = (mousex > 500 && mousey > 386 && mousex < 718 && mousey <434) ? (back_B) :((mousex > 481 && mousey > 499 && mousex < 741 && mousey <547) ? (back_C) : ((mousex > 516 && mousey > 607 && mousex < 713 && mousey < 655) ? (back_D): (back_A)));
+		background = (mousex > 497 && mousey > 337 && mousex < 719 && mousey <384) ? (back_B) :((mousex > 481 && mousey > 440 && mousex < 737 && mousey <487) ? (back_C) : ((mousex > 384 && mousey > 541 && mousex < 860 && mousey < 589) ? (back_D): ((mousex > 521 && mousey > 648 && mousex < 719 && mousey < 696) ? (back_E) : (back_A))));
 		al_draw_bitmap(background, 0, 0, 0);
 		al_flip_display();
 	}
@@ -391,6 +452,8 @@ void inicializar() {
 	}
 	imgEnems[0][0]= al_load_bitmap("enemigoBola.png");
 	imgEnems[1][0] = al_load_bitmap("enemigoPulpo.png");
+	imgEnems[0][1] = al_load_bitmap("enemigoBolaD.png");
+	imgEnems[1][1] = al_load_bitmap("enemigoPulpoD.png");
 }
 
 int main()
